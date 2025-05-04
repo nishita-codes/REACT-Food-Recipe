45f80adc7902c6479d40c8c74f6fd0ab85e45dcb
Nishita-codes <nishitapatidar03@gmail.com>
2025-05-04 23:13:02 +0530

    added files

--- src/Component/MainPage.jsx
+++ src/Component/MainPage.jsx
@@ -5,25 +5,42 @@ import MealCards from './MealCards';
 
 const MainPage = () => {
   const [data,setData] = useState();
-  const [search ,setSearch] =useState();
+  const [search ,setSearch] =useState("");
+  const [msg,setMsg] = useState("");
+  const [notFound, setNotFound] = useState(false);
 
   const handleInput =(event)=>{
     setSearch(event.target.value)
   }
 
   const myFun = async () => {
-    try {
+    if(search == ""){
+      setMsg("Please Enter Something!:)");
+      setNotFound(false);
+    }else{
+    try{
       const get = await fetch(`https://www.themealdb.com/api/json/v1/1/search.php?s=${search}`);
       const data = await get.json();
       // console.log(data); 
-      setData(data.meals)
-    } catch (error) {
-      console.error("Error fetching data:", error);
+      if (data && data.meals && data.meals.length > 0) {
+        setData(data.meals);
+        setNotFound(false); // Reset notFound when results are found
+      } else {
+        setData([]); // Clear any previous data
+        setNotFound(true);  // Set notFound to true when no results are found
+      }
+      setMsg("");
+    }catch(error){
+      setData([]);
+      setNotFound(true);
+      setMsg("Error fetching data. Please try again later.");
     }
+  }
   };
   
   return (
     <>
+    <h1 className='head'>FOOD RECIPE APP</h1>
       <div className='container'>
         <div className="searchbar">
           <div className='Search'>
@@ -32,6 +49,8 @@ const MainPage = () => {
           </div>
             <button className='btn' onClick={myFun}> Search</button>
         </div>
+        <h4 className='msg'>{msg}</h4>
+        <h4>{notFound && <p className='notFound'>Data Not Found! Please try a different search term.</p>}</h4>
         <div>
             <MealCards detail={data}/>
             </div>
